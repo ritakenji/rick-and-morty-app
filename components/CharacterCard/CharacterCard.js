47546cc2a322b71a6276f97e073c308d7c4661cf
Rita <rita.kenji@gmail.com>
2025-08-21 16:08:58 +0200

    add relevant HTML code of the card from the index.html to createCharacterCard function

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,3 +1,31 @@
+import { createElement } from "react";
+
 export default function createCharacterCard() {
-    const card = 
+  const card = document.createElement("li");
+  card.classList.add("card");
+  card.innerHTML =
+    '<div class="card__image-container">' +
+    `<img class="card__image" src=" ${imageSrc} " alt=""/>` +
+    '<div class="card__image-gradient">' +
+    "</div>" +
+    "</div>" +
+    '<div class="card__content">' +
+    '<h2 class="card__title">' +
+    `${characterName}` +
+    "</h2>" +
+    '<dl class="card__info">' +
+    '<dt class="card__info-title">Status</dt>' +
+    '<dd class="card__info-description">' +
+    `${characterDescription}` +
+    "</dd>" +
+    '<dt class="card__info-title">Type</dt>' +
+    '<dd class="card__info-description">' +
+    `${characterType}` +
+    "</dd>" +
+    '<dt class="card__info-title">Occurrences</dt>' +
+    '<dd class="card__info-description">' +
+    `${characterOccurrence}` +
+    "</dd>" +
+    "</dl>" +
+    "</div>";
 }
