36a6844119b7d3e32b64d12f98443134892c1bb0
Rita Macedo <rita.kenji@gmail.com>
2025-08-22 16:05:29 +0200

    Revert "Refactoring"

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-import { createCharacterCard } from "./components/CharacterCard/CharacterCard.js";
-import { handleSubmitButtons } from "./components/NavButton/NavButton.js";
-import { handleSearchBar } from "./components/SearchBar/SearchBar.js";
-import { pagination } from "./components/NavPagination/NavPagination.js";
+import createCharacterCard from "./components/CharacterCard/CharacterCard.js";
 
-export const cardContainer = document.querySelector(
-  '[data-js="card-container"]'
+const cardContainer = document.querySelector('[data-js="card-container"]');
+const searchBarContainer = document.querySelector(
+  '[data-js="search-bar-container"]'
 );
+const searchBar = document.querySelector('[data-js="search-bar"]');
+const navigation = document.querySelector('[data-js="navigation"]');
+const prevButton = document.querySelector('[data-js="button-prev"]');
+const nextButton = document.querySelector('[data-js="button-next"]');
+const pagination = document.querySelector('[data-js="pagination"]');
 
-export let currentPage = 1; // changed to let cause const cant be reassigned
-export let maxPages; //needs to be able to change
-export let searchQuery = "";
+// States
+let currentPage = 1; // changed to let cause const cant be reassigned
+let maxPages; //needs to be able to change
+let searchQuery = "";
 
-export async function fetchCharacters(currentPage) {
+async function fetchCharacters(currentPage) {
   cardContainer.innerHTML = " ";
-  const url = `https://rickandmortyapi.com/api/character?page=${currentPage}&name=${searchQuery}`;
+  const url = `https://rickandmortyapi.com/api/character?page=${currentPage}&name=${searchQuery}`; //this needs to be tested after merging
   try {
     const response = await fetch(url); //Once this promise resolves (the network request is finished), we call the .json method on the response variable.
     if (!response.ok) {
@@ -21,9 +25,8 @@ export async function fetchCharacters(currentPage) {
     }
     const data = await response.json(); //This second promise resolves with the actual data (payload) converted from JSON (a formatted string) to a JavaScript value or object. This result is stored in the variable named data.
     maxPages = data.info.pages;
-
+    pagination.textContent = `${currentPage} / ${maxPages}`;
     // ***************************************** GET ELEMENTS & CREATE NEW CARD **************************************************************
-    //  ↓ ↓ ↓ ↓ ↓ ↓ needs to be refactored
     data.results.forEach((element) => {
       const {
         image: imageSrc,
@@ -42,10 +45,8 @@ export async function fetchCharacters(currentPage) {
       cardContainer.append(newCard);
     });
 
-    pagination.textContent = `${currentPage} / ${maxPages}`;
-    console.log(currentPage);
-
     // *********************************************** ERROR HANDLING **************************************************************
+
     //return data;we don't need it
   } catch (error) {
     console.error("Fetch error:", error.message);
@@ -54,7 +55,65 @@ export async function fetchCharacters(currentPage) {
   }
 }
 
-handleSubmitButtons();
-handleSearchBar();
+// *********************************************** PAGINATION *****************************************************************
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> VERSION 1 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+/* fetchCharacters().then((data) => {
+let maxPages = data.info.pages; //needs to be able to change
+fetchCharacters();
+prevButton.addEventListener("click", () => {
+  if (currentPage > 1) {
+    currentPage--;
+    fetchCharacters();
+    pagination.textContent = `${currentPage} / ${maxPages}`;
+  } else {
+    prevButton.style.display = "none"; //this doesnt seem to work, probably because the button is already there on the html
+    //prevButton.setAttribute("disabled", true); //another option
+  }
+});
+nextButton.addEventListener("click", () => {
+  if (currentPage < maxPages) {
+    currentPage++;
+    fetchCharacters();
+    pagination.textContent = `${currentPage} / ${maxPages}`;
+  } else {
+    nextButton.style.display = "none"; //will test this after merging because im not about to click the button 42 times to find out lol
+  }
+}); */
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> OR VERSION 2 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 fetchCharacters(currentPage);
+prevButton.addEventListener("click", () => {
+  if (currentPage > 1) {
+    currentPage--;
+    fetchCharacters(currentPage);
+    pagination.textContent = `${currentPage} / ${maxPages}`;
+  } else {
+    prevButton.style.display = "none"; //this doesnt seem to work, probably because the button is already there on the html
+    //prevButton.setAttribute("disabled", true); //another option
+  }
+});
+nextButton.addEventListener("click", () => {
+  if (currentPage < maxPages) {
+    currentPage++;
+    fetchCharacters(currentPage);
+    pagination.textContent = `${currentPage} / ${maxPages}`;
+  } else {
+    nextButton.style.display = "none"; //will test this after merging because im not about to click the button 42 times to find out lol
+  }
+});
+searchBar.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  searchQuery = data.query;
+  console.log("searchQuery :", searchQuery);
+  try {
+    await fetchCharacters(currentPage);
+  } catch (error) {
+    console.error("Fetch error:", error.message);
+    cardContainer.innerHTML = `<p>Error: ${error.message}</p>`;
+  }
+});
